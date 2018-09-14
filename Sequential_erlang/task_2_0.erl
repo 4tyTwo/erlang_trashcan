c33979a64d6231e59f872477b2789114c5da0212
Toporkov Igor <i.toporkov@rbkmoney.com>
2018-09-14 12:49:22 +0300

    Added missing whitespaces and removed guard

--- Sequential_erlang/task_2_0.erl
+++ Sequential_erlang/task_2_0.erl
@@ -1,30 +1,35 @@
 -module(task_2_0).
--export([seq/2,seq/3,filter/2,zip/2,test/0]).
 
-seq(Start,Finish) ->
-  seq(Start,Finish,1).
+-export([test/0]).
+-export([seq/2]).
+-export([seq/3]).
+-export([zip/2]).
+-export([filter/2]).
 
-seq(_,_,Step) when Step == 0 ->
+seq(Start, Finish) ->
+  seq(Start, Finish, 1).
+
+seq(_,_,0) ->
   erlang:error(badarg,[Step]);
-seq(Start,Finish,Step) when Start > Finish, Step > 0 ->
-  erlang:error(badarg,[Start,Finish,Step]);
-seq(Start,Finish,Step) when Start < Finish, Step < 0 ->
-  erlang:error(badarg,[Start,Finish,Step]);
-seq(Start,Finish,Step) ->
-  seq(Start,Finish,Step,[]).
+seq(Start, Finish, Step) when Start > Finish, Step > 0 ->
+  erlang:error(badarg,[Start, Finish, Step]);
+seq(Start, Finish, Step) when Start < Finish, Step < 0 ->
+  erlang:error(badarg,[Start, Finish, Step]);
+seq(Start, Finish, Step) ->
+  seq(Start, Finish, Step,[]).
 
-seq(Start,Finish,Step,Acc) when Start > Finish, Step > 0->
+seq(Start, Finish, Step, Acc) when Start > Finish, Step > 0->
   lists:reverse(Acc);
-seq(Start,Finish,Step,Acc) when Start < Finish, Step < 0->
+seq(Start, Finish, Step, Acc) when Start < Finish, Step < 0->
   lists:reverse(Acc);
 seq(Start,Finish,Step,Acc) ->
-  seq(Start + Step,Finish,Step,[Start | Acc]).
+  seq(Start + Step, Finish, Step, [Start | Acc]).
 
 
-filter(Fun,List) ->
-  filter(Fun,List,[]).
+filter(Fun, List) ->
+  filter(Fun, List, []).
 
-filter(_,[],Acc) ->
+filter(_, [], Acc) ->
   lists:reverse(Acc);
 filter(Fun, [Head | Tail],Acc) ->
   case Fun(Head) of
@@ -36,29 +41,29 @@ filter(Fun, [Head | Tail],Acc) ->
 
 %Написанный zip поддерживает списки разной длины
 zip(List1, List2) ->
-  zip(List1,List2,[]).
+  zip(List1, List2, []).
 
-zip([],_,Result) ->
+zip([], _, Result) ->
   lists:reverse(Result);
-zip(_,[],Result) ->
+zip(_, [], Result) ->
   lists:reverse(Result);
-zip([Head1 | Tail1],[Head2 | Tail2],Result) ->
-  zip(Tail1,Tail2,[{Head1,Head2} | Result]).
+zip([Head1 | Tail1], [Head2 | Tail2], Result) ->
+  zip(Tail1, Tail2, [{Head1,Head2} | Result]).
 
 
 test() ->
   %Seq test
-  Sample = lists:seq(1,10),
-  Sample = seq(1,10),
-  Sample1 = lists:seq(1,-10,-2),
-  Sample1 = seq(1,-10,-2),
+  Sample = lists:seq(1, 10),
+  Sample = seq(1, 10),
+  Sample1 = lists:seq(1, -10, -2),
+  Sample1 = seq(1, -10, -2),
   %Filter test
   GT5 = fun(X) -> X > 5 end,
-  SeqSample = lists:filter(GT5,Sample),
-  SeqSample = filter(GT5,Sample),
+  SeqSample = lists:filter(GT5, Sample),
+  SeqSample = filter(GT5, Sample),
   %Zip test
   A = [1,2,3],
   B = [a,b,c],
-  ZipSample = lists:zip(A,B),
-  ZipSample = zip(A,B),
-  io:format("Test passed~n").
+  ZipSample = lists:zip(A, B),
+  ZipSample = zip(A, B),
+  ok = io:fwrite("Test passed~n").
